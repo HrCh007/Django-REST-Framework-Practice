afe0d171fdbd207271b4a9c2759b5e7bcccdf5d2
HrCh007 <chowdhryhrithik@gmail.com>
2022-08-19 19:35:04 +0530

    added model viewsets

--- api_basic/views.py
+++ api_basic/views.py
@@ -15,8 +15,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
-class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,
-                    mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer                         
     queryset = Article.objects.all()
 
