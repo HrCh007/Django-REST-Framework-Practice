264f4ae13349812854503eb80f0f450f9b2c40ff
HrCh007 <chowdhryhrithik@gmail.com>
2022-08-19 15:28:49 +0530

    added basic and session authentication

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, ArticleListGenericAPIView, ArticleDetailGenericAPIView
 
 urlpatterns = [
-    #path('article/', article_list),
-    path('article/', ArticleAPIView.as_view()),
-    #path('detail/<int:pk>/', article_detail),
-    path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('article/', article_list),
+    #path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:pk>/', article_detail),
+    #path('detail/<int:id>/', ArticleDetails.as_view()),
     path('generic/article/', ArticleListGenericAPIView.as_view()),
-    path('generic/detail/<int:id>/', ArticleDetailGenericAPIView.as_view())
+    path('generic/article/<int:id>/', ArticleDetailGenericAPIView.as_view())
 ]
